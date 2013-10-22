844a037b71077239f0b86e8b2c473e7b138e6408
Xiaohe Hu <huxiaohe113@gmail.com>
2013-10-22 10:07:13 -0400

    Initial Commit

---
+++ xhAutoLayoutTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  xhAutoLayoutTest
+//
+//  Created by Xiaohe Hu on 10/22/13.
+//  Copyright (c) 2013 Xiaohe Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ xhAutoLayoutTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  xhAutoLayoutTest
+//
+//  Created by Xiaohe Hu on 10/22/13.
+//  Copyright (c) 2013 Xiaohe Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
